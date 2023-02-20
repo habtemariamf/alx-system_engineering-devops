7a01a7f29fbf7081e54d515661ce4bae57614dcd
habtemariamf <habte21fb@gmail.com>
2023-02-19 23:49:30 -0800

    all directories to git

---
+++ root/alx-low_level_programming/0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* * main - main block
+* * Use `putchar` function to print the alphabet in lowercase.
+* * Return: 0
+* *
+*/
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
+}
---
+++ root/alx-low_level_programming/0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* malloc_checked - Entry Point
+* @b: input amount
+* Return: pointer to new mem
+*/
+void *malloc_checked(unsigned int b)
+{
+void *mem = malloc(b);
+
+{
+if (mem == NULL)
+exit(98);
+}
+return (mem);
+}
